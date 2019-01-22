0940380cc17030d4a755dbf65f22b62e4441189d
Javier Aznar <jaznar@idealista.com>
2019-01-22 11:39:28 +0100

    Update header files

---
+++ Galileo-iOS/Classes/Plugins/Galileo-iOS.h
@@ -0,0 +1,12 @@
+//
+//  Galileo-iOS.h
+//  Galileo-iOS
+//
+//  Created by Javier Aznar on 22/01/2019.
+//
+
+#ifndef Galileo_iOS_h
+#define Galileo_iOS_h
+
+
+#endif /* Galileo_iOS_h */
---
+++ Galileo-iOS/Classes/Plugins/Wormholy/Sources/Objc/NSURLSessionConfiguration+Wormholy.h
@@ -0,0 +1,12 @@
+//
+//  NSURLSessionConfiguration+Wormholy.h
+//  Galileo-iOS
+//
+//  Created by Javier Aznar on 22/01/2019.
+//
+
+#ifndef NSURLSessionConfiguration_Wormholy_h
+#define NSURLSessionConfiguration_Wormholy_h
+
+
+#endif /* NSURLSessionConfiguration_Wormholy_h */
