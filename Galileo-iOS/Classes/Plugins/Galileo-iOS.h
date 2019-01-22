f0356a2d52d2abb7f08d06b9de5f1eacf89dd628
Javier Aznar <jaznar@idealista.com>
2019-01-22 11:39:33 +0100

    Update header files

--- Galileo-iOS/Classes/Plugins/Galileo-iOS.h
+++ Galileo-iOS/Classes/Plugins/Galileo-iOS.h
@@ -5,8 +5,22 @@
 //  Created by Javier Aznar on 22/01/2019.
 //
 
-#ifndef Galileo_iOS_h
-#define Galileo_iOS_h
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
 
 
-#endif /* Galileo_iOS_h */
+#import "NSURLSessionConfiguration+Wormholy.h"
+#import "WormholyMethodSwizzling.h"
+
+
+FOUNDATION_EXPORT double WormholyVersionNumber;
+FOUNDATION_EXPORT const unsigned char WormholyVersionString[];
